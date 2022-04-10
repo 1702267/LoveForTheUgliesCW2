5548a2a7d0082ff9c235fc99d283c63885e66702
1702267 <1702267@rgu.ac.uk>
2022-04-10 22:40:28 +0100

    added models folder and usermodel file

--- server/express.js
+++ server/express.js
@@ -5,7 +5,7 @@ import cookieParser from 'cookie-parser'
 import compress from 'compression'
 import cors from 'cors'
 import helmet from 'helmet'
-
+import template from 'C:\Users\Acer Nitro 5 SSD\Documents\Uni Stuff\4th Year\Enterprise Web Systems\LoveForTheUgliesCW2\LoveForTheUgliesCW2\template'
 const app = express()
 
 // parse body params and attache them to req.body
@@ -17,6 +17,10 @@ app.use(compress())
 app.use(helmet())
 // enable CORS - Cross Origin Resource Sharing
 app.use(cors())
+
+app.get('/',(req, res) => {
+  res.status(200).send(Template())
+})
 
 
 export default app
