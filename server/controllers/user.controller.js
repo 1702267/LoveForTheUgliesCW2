7f4ca565e35d653919ee5e22028eb105430e7c1d
1702267 <1702267@rgu.ac.uk>
2022-04-10 23:06:27 +0100

    added routes and controllers

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,30 @@
+import User from '../models/user.model'
+import extend from 'lodash/extend'
+import errorHandler from './../helpers/dbErrorHandler'
+
+const create = async (req, res) => {
+}
+
+const list = async (req, res) => {
+}
+
+const userByID = async (req, res, next, id) => {
+}
+
+const read = (req, res) => {
+}
+
+const update = async (req, res) => {
+}
+
+const remove = async (req, res) => {
+}
+
+export default {
+ create,
+ userByID,
+ read,
+ list,
+ remove,
+ update
+}
