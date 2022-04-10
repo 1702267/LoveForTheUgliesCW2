0aee4f2007d9f5bba145c94c70872eaf4f5949bb
1702267 <1702267@rgu.ac.uk>
2022-04-10 21:10:31 +0100

    the database folder exists now, theres nothing in here get tho im still messing with mongo and stuff

--- server/server.js
+++ server/server.js
@@ -9,6 +9,15 @@ mongoose.connection.on('error', err => {
  throw new Error(`unable to connect to database: ${config.mongoUri}`)
 })
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://LFTUadmin:love4ugly@lftuuserdb.nxm6d.mongodb.net/LFTUuserDB?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 app.listen(config.port, (err) => {
  if (err) {
  console.log(err)
